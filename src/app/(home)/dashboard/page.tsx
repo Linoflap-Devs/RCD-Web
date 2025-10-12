452f6877ea12ec29138d74b0c0bdd77f7b29a2e7
staneunbl <shayyannelee@gmail.com>
2025-10-13 07:37:39 +0800

    corrected build issues

--- src/app/(home)/dashboard/page.tsx
+++ src/app/(home)/dashboard/page.tsx
@@ -1,10 +1,13 @@
 import AccessGuard from "@/components/pages/AccessGuard";
 import Dashboard from "@/components/pages/Dashboard";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
-    <AccessGuard>
-      <Dashboard />
-    </AccessGuard>
+    <Suspense fallback={<div>Loading dashboard...</div>}>
+      <AccessGuard>
+        <Dashboard />
+      </AccessGuard>
+    </Suspense>
   )
 }
